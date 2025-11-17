a6fb5c5f95e36cfa9d98e32758070b669d88847f
edupoch <edupoch@gmail.com>
2025-11-17 10:06:30 +0100

    Url del site en producción

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  site: import.meta.env.PROD
+    ? "https://estudiocaravana.github.io/galiciaspa25/"
+    : undefined,
 });
